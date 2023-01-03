2be8ace2e51bcad1a60c84dc7ed1f834c27eb7b2
akku1298 <akanshasrivastav1212@gmail.com>
2023-01-03 15:50:52 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,9 @@
+const heading= React.createElement(
+    "h1",{
+     id:"title",
+    },
+    "Heading 1"
+)
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
